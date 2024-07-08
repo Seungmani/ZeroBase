6cc3678dc2c3d01f438a7a27f92f514b9ac8a125
Seungmani <dkelcncjs@naver.com>
2024-07-08 16:13:14 +0900

    [feat] toggle 이벤트 완성

--- JS/과제-1/template/app.js
+++ JS/과제-1/template/app.js
@@ -1,1 +1,17 @@
 // do something!
+const $toggle = document.querySelector(".toggle");
+const $nav = document.querySelector("nav");
+const $preload = document.querySelector(".preload");
+
+// 토글 클릭 이벤트
+const clickToggle = () => {
+  $nav.classList.toggle("active");
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  $nav.classList.toggle("active");
+  document.body.style.visibility = "visible";
+	document.body.classList.remove("preload");
+});
+
+$toggle.addEventListener("click", clickToggle);
