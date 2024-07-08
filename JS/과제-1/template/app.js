08d1a60bfff70df8f3a0decfa0d3de7e792fba80
Seungmani <dkelcncjs@naver.com>
2024-07-08 17:11:50 +0900

    [feat] localStorage를 통해 nav 상태 저장 완성

--- JS/과제-1/template/app.js
+++ JS/과제-1/template/app.js
@@ -3,15 +3,37 @@ const $toggle = document.querySelector(".toggle");
 const $nav = document.querySelector("nav");
 const $preload = document.querySelector(".preload");
 
-// 토글 클릭 이벤트
-const clickToggle = () => {
-  $nav.classList.toggle("active");
+// nav 상태 저장 
+const saveNavOpenState = (isOpen) => {
+	localStorage.setItem('sidenavState', isOpen ? 'open' : 'closed');
+}
+
+// nav 상태 불러오기
+const getNavState = () => {
+	return localStorage.getItem("sidenavState");
+}
+
+const changeNavState = () => {
+	$nav.classList.toggle("active");
+	saveNavOpenState($nav.classList.contains("active"));
 };
 
+const showNav = (state) => {
+	if (state === "open") $nav.classList.add("active");
+  else $nav.classList.remove("active");
+}
+
+const initialSetting = () => {
+	document.body.style.visibility = "visible";
+	requestAnimationFrame(() => {
+		document.body.classList.remove("preload");
+	});
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  $nav.classList.toggle("active");
-  document.body.style.visibility = "visible";
-	document.body.classList.remove("preload");
+	const openState = getNavState();
+	showNav(openState);
+	initialSetting();
 });
 
-$toggle.addEventListener("click", clickToggle);
+$toggle.addEventListener("click", changeNavState);
