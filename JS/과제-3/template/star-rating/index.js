63e91dec73fbd2ed34d5bec1db52a6f2f542deae
Seungmani <dkelcncjs@naver.com>
2024-07-16 22:26:12 +0900

    [feat] hover, click 커스텀 이벤트 생성

--- JS/과제-3/template/star-rating/index.js
+++ JS/과제-3/template/star-rating/index.js
@@ -13,6 +13,50 @@ const makeStars = (num, div) => {
 const StarRating = (container) => {
   makeStars(Number(container.dataset.maxRating), container);
 
+	const $stars = container.querySelectorAll(".bx");
+
+  // 별점을 클릭했을 때 이벤트 핸들러
+  const handleRating = (event) => {
+    const value = event.target.dataset.value;
+    if (value) {
+      // 별점 상태 업데이트
+      $stars.forEach(star => {
+        star.classList.toggle('selected', star.dataset.value <= value);
+      });
+
+      // rating-change 이벤트 생성 및 발생
+      const customEvent = new CustomEvent('rating-change', {
+        detail: value
+      });
+      container.dispatchEvent(customEvent);
+    }
+  };
+
+  // 별점 호버 이벤트 핸들러
+  const handleHover = (event) => {
+    const value = event.target.dataset.value;
+    if (value) {
+      // 호버된 별점 상태 업데이트
+      $stars.forEach(star => {
+        star.classList.toggle('hovered', star.dataset.value <= value);
+      });
+    }
+  };
+
+  // 별점 호버 아웃 이벤트 핸들러
+  const handleHoverOut = () => {
+    // 호버 상태 제거
+    $stars.forEach(star => {
+      star.classList.remove('hovered');
+    });
+  };
+
+  // 클릭 이벤트 리스너 추가
+  container.addEventListener('click', handleRating);
+
+  // 호버 이벤트 리스너 추가
+  container.addEventListener('mouseover', handleHover);
+  container.addEventListener('mouseout', handleHoverOut);
 };
 
 export default StarRating;
