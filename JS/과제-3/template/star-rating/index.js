8c97193fc89c8ee06c42ca3583d402cc957ffb71
Seungmani <dkelcncjs@naver.com>
2024-07-16 22:24:44 +0900

    [feat] data-rating에 따른 별 그리는 함수

--- JS/과제-3/template/star-rating/index.js
+++ JS/과제-3/template/star-rating/index.js
@@ -1,1 +1,18 @@
 // do something!
+const makeStars = (num, div) => {
+	const star = document.createElement('span');
+	star.className = 'star-rating-container';
+
+  for (let i = 0; i < num; i++) {
+    star.innerHTML += `<i class='bx bxs-star' data-value=${i + 1}></i>`;
+  }
+
+	div.appendChild(star);
+};
+
+const StarRating = (container) => {
+  makeStars(Number(container.dataset.maxRating), container);
+
+};
+
+export default StarRating;
